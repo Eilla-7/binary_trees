80dd9abce1dbc4381d7785d0db87fb3ac90aedad
mojtaba <mojtaba.mohamed187@gmail.com>
2024-01-30 18:52:34 +0200

    fix linting

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -19,7 +19,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 {
 	size_t nodes = 0;
 
-    if (tree != NULL)
+	if (tree != NULL)
 	{
 		nodes += tree->left || tree->right ? 1 : 0;
 		nodes += binary_tree_nodes(tree->left);
