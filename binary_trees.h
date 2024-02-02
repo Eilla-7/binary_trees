21602c29bd41bce5cf9a953addf3437efafdbef2
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-02 04:54:28 +0200

    fix linting

--- binary_trees.h
+++ binary_trees.h
@@ -92,7 +92,6 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 		const binary_tree_t *second);
 binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-
 int binary_tree_is_bst(const binary_tree_t *tree);
 int is_binary_search_tree(const binary_tree_t *tree, int min, int max);
 void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
