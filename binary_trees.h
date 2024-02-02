e3ac7f67532893faeae99c2a41ce6db9a207f00d
mojtaba <mojtaba.mohamed187@gmail.com>
2024-02-02 04:55:50 +0200

    fix linting

--- binary_trees.h
+++ binary_trees.h
@@ -64,9 +64,12 @@ typedef struct complete_queue_s complete_queue_t;
 
 
 /* ============ PRINT FUNCTION ============ */
+
 void binary_tree_print(const binary_tree_t *tree);
 
+
 /* ============ PROJECT FUNCTIONS ============ */
+
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
